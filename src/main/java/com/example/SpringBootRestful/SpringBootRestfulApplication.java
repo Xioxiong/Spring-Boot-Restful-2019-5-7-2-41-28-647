42cd5ce32d7831bcc2402499fdff531f0b11df20
xiongwei <2894001817@qq.com>
2019-07-26 17:02:07 +0800

    完成了增删查改，图片在根目录下的img文件夹下

--- src/main/java/com/example/SpringBootRestful/SpringBootRestfulApplication.java
+++ src/main/java/com/example/SpringBootRestful/SpringBootRestfulApplication.java
@@ -8,6 +8,7 @@ public class SpringBootRestfulApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootRestfulApplication.class, args);
+		System.out.println("火箭即将升空，请各单位做好准备！");
 	}
 
 }
--- src/main/java/com/example/SpringBootRestful/controller/UserController.java
+++ src/main/java/com/example/SpringBootRestful/controller/UserController.java
@@ -9,36 +9,40 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("")
+@RequestMapping("/user")
 //ToDo:在上一行代码("")中填写合适的RestfulApi
 public class UserController {
 
     @Autowired
     private UserService userService;
 
-    @GetMapping("")
+    @GetMapping("/get")
     //ToDo:在上一行代码("")中填写合适的RestfulApi
     public ResponseEntity getAllUser() {
+        System.out.println("进入了get方法");
         return new ResponseEntity<>(userService.getAllUser(), HttpStatus.OK);
     }
 
-    @PostMapping("")
+    @PostMapping("/add")
     //ToDo:在上一行代码("")中填写合适的RestfulApi
     public ResponseEntity addUser(@RequestBody User user) {
+        System.out.println("进入了add方法");
         userService.addUser(user);
         return new ResponseEntity(HttpStatus.CREATED);
     }
 
-    @PutMapping("")
+    @PutMapping("/update/{userId}")
     //ToDo:在上一行代码("")中填写合适的RestfulApi
     public ResponseEntity updateUserById(@PathVariable int userId, @RequestBody User user) throws BusinessException {
+        System.out.println("进入了update方法");
         userService.updateUserById(userId, user);
         return new ResponseEntity(HttpStatus.NO_CONTENT);
     }
 
-    @DeleteMapping("")
+    @DeleteMapping("/delete/{userId}")
     //ToDo:在上一行代码("")中填写合适的RestfulApi
     public ResponseEntity deleteUserById(@PathVariable int userId) throws BusinessException {
+        System.out.println("进入了delete方法");
         userService.deleteUserById(userId);
         return new ResponseEntity(HttpStatus.NO_CONTENT);
     }
